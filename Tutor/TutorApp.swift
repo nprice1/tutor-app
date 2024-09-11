d523d22e8be53ca72f409a9a0a689c5324f53718
Nolan Price <nolan.price@sheerid.com>
2024-09-11 13:34:51 -0700

    Initial Commit

---
+++ Tutor/TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorApp.swift
+//  Tutor
+//
+//  Created by Nolan Price on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
